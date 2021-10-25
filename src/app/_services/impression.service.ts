97be4ae63ce4b939149e89d8ee724e63e241cf77
MajidAy <majidemazaheri@gmail.com>
2021-10-25 10:58:31 +0200

    added code for import file

--- src/app/_services/impression.service.ts
+++ src/app/_services/impression.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import * as XLSX from 'xlsx';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ImpressionService {
+  constructor() {}
 
-  constructor() { }
+  data = [];
+
+  uploadedFile(bstr: string): any {
+    const wb: XLSX.WorkBook = XLSX.read(bstr, { type: 'binary' });
+
+    const wsName: string = wb.SheetNames[0];
+
+    const ws: XLSX.WorkSheet = wb.Sheets[wsName];
+
+    this.data = XLSX.utils.sheet_to_json(ws, { raw: true });
+    return this.data;
+  }
 }
--- src/app/components/home/home/home.component.ts
+++ src/app/components/home/home/home.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { ImpressionService } from 'src/app/_services/impression.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  data = [];
+  constructor(private service: ImpressionService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  uploadedFile = (event: any) => {
+    const target: DataTransfer = event.target as DataTransfer;
+    const reader: FileReader = new FileReader();
+    reader.onload = (e: any) => {
+      const bstr: string = e.target.result;
+      this.data = this.service.uploadedFile(bstr);
+      console.log('data', this.data);
+    };
+    reader.readAsBinaryString(target.files[0]);
   }
-
 }
